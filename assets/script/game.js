9228875566abc1eefcdd99d207ee97166b134b48
blakeyi <1152420971@qq.com>
2022-09-22 00:27:55 +0800

    chore:完成基本动画, 有个bug是拉到的牛和显示的不一致, randomType的问题

--- assets/script/game.js
+++ assets/script/game.js
@@ -9,6 +9,14 @@ cc.Class({
         cow_node:{
             default:null,
             type:cc.Node
+        },
+        rope_image:{
+            default:[],
+            type:cc.SpriteFrame
+        },
+        cow_prefab:{
+            default:null,
+            type:cc.Prefab
         }
     },
 
@@ -30,12 +38,32 @@ cc.Class({
             let currentX = this.cow_node.x
             if (currentX > -100 && currentX < 100) {
                 cc.log("捕捉成功")
+                
+                // 替换rope_node
+                let ropeType = this.cow_node.getComponent("cow").randomType + 1
+                cc.log(ropeType)
+                this.rope_node.getComponent(cc.Sprite).spriteFrame = this.rope_image[ropeType]
+                
+                // 移除正在奔跑的牛儿
+                let bgNode = this.node.getChildByName("BgSprite")
+                bgNode.removeChild(this.cow_node)
+                
+                // 生成新的牛节点
+                this.cow_node = cc.instantiate(this.cow_prefab)
+                this.cow_node.y = 0
+                bgNode.addChild(this.cow_node)
             } else {
                 cc.log("捕捉失败")
             }
         }, this)
         
-        let sequence = cc.sequence(up, result)
+        // 绳子收回来
+        let down = cc.moveTo(0.5, this.rope_node.x, -750)
+        
+        let finish = cc.callFunc(function(){
+            this.rope_node.getComponent(cc.Sprite).spriteFrame = this.rope_image[0]
+        },this)
+        let sequence = cc.sequence(up, result, down,finish)
         this.rope_node.runAction(sequence)
     }
 });
